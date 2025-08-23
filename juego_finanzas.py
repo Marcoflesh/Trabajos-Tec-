6fb9a50d4dd66e1cbc6272608718515680bf8c7f
Marcoflecha <istacu29@gmail.com>
2025-08-22 21:34:41 -0600

    juego_finanzas.py

--- juego_finanzas.py
+++ juego_finanzas.py
@@ -6,10 +6,13 @@ Generar un número aleatorio como salario del día 1 utilizando variables de ale
 Conforme pasan los días los números aleatorios serán un cierto porcentaje menor al inicial
 Mientras el protagonista esté vivo y con dinero mayor a 0 seguirá el juego
 si el godín llega a 0 o si no termina la semana debidamente, se termina el juego y GAME OVER Entrada final
-Al final de cada jornada se mostrarán opciones de pago que se seleccionarán con las flechitas del teclado\n o las teclas WASD
+Al final de cada jornada se mostrarán opciones de pago que se seleccionarán con las flechitas del teclado\n 
+o las teclas WASD
 El jugador eligirá la opción que más le agrade para seguir con el juego y mantener al godín vivo
 se le restará el monto de su selección a su cuenta de ingresos
 si la opción que escogió pone el dinero en 0, (se terminará el juego y perderá el usuario) Entrada final
 Fin mientras
-Si el usuario llega a terminar la semana con un buen manejo del dinero del godín habrá ganado la run y \n saldrá un video de evangelion diciendo "FELICIDADES SHINJI" Entrada final
+Si el usuario llega a terminar la semana con un buen manejo del dinero del godín habrá ganado la run y \n
+saldrá un video de evangelion diciendo "FELICIDADES SHINJI" Entrada final
+
 En caso de que no termine la run, saldrá un Texto que diga "GAME OVER" y vuelve desde el inicio
